b4ed2cccdba1830d76714fcf0def8c89d0451233
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2018-10-08 11:42:50 -0300

    <@ingridDuarte, @eduarda96, @brendamoniqueb, @laurascclaro> Implementa formulário

--- app/src/main/java/com/thoughtworks/aceleradora/solicitacao/dominio/Solicitacao.java
+++ app/src/main/java/com/thoughtworks/aceleradora/solicitacao/dominio/Solicitacao.java
@@ -14,14 +14,38 @@ public class Solicitacao {
     private String nome;
     @OneToOne(cascade=ALL)
     private Endereco endereco;
+    private String genero;
+    private String situacao;
+    private String generoAcomp1;
+    private String generoAcomp2;
 
     public Solicitacao() {
     }
 
-    public Solicitacao(String nome, Endereco endereco) {
+    public Solicitacao(String nome, Endereco endereco, String genero, String situacao, String generoAcomp1, String generoAcomp2) {
         this.nome = nome;
         this.endereco = endereco;
+        this.genero = genero;
+        this.situacao = situacao;
+        this.generoAcomp1 = generoAcomp1;
+        this.generoAcomp2 = generoAcomp2;
 
+    }
+
+    public String getGeneroAcomp2() {
+        return generoAcomp2;
+    }
+
+    public void setGeneroAcomp2(String generoAcomp2) {
+        this.generoAcomp2 = generoAcomp2;
+    }
+
+    public String getGeneroAcomp1() {
+        return generoAcomp1;
+    }
+
+    public void setGeneroAcomp1(String generoAcomp1) {
+        this.generoAcomp1 = generoAcomp1;
     }
 
     public void setNome(String nome) {
@@ -40,5 +64,19 @@ public class Solicitacao {
         return nome;
     }
 
+    public String getGenero() {
+        return genero;
+    }
 
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
 }
---
+++ app/src/main/resources/db/migration/V8__Alter_solicitacao_table_generoAcomp1.sql
@@ -0,0 +1,1 @@
+alter table solicitacao add column generoAcomp1 varchar;
