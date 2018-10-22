dc0d04fb12a4e8a903cd7b78e121f5782249eac8
diovanemendes <mmaatheeus@gmail.com>
2018-10-22 09:47:36 -0300

    <@ingridDuarte,@diovannemendes> alterações do code Review

--- app/src/main/java/com/thoughtworks/aceleradora/solicitacao/controller/SolicitacaoController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/solicitacao/controller/SolicitacaoController.java
@@ -1,7 +1,5 @@
 package com.thoughtworks.aceleradora.solicitacao.controller;
 
-import com.thoughtworks.aceleradora.solicitacao.dominio.ListaSolicitacaoCasaRepository;
-import com.thoughtworks.aceleradora.solicitacao.dominio.ListaSolicitacaoHospitalRepository;
 import com.thoughtworks.aceleradora.solicitacao.dominio.Solicitacao;
 import com.thoughtworks.aceleradora.solicitacao.dominio.SolicitacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/solicitacao")
@@ -42,7 +37,7 @@ public class SolicitacaoController {
         
         model.addAttribute("solicitacoesCasa", repositorioSolicitacao.findAll());
 
-        return "listaSolicitacao/listaSolicitacaoCasa";
+        return "solicitacao/listagens/listaSolicitacaoCasa";
     }
 
     @GetMapping("/hospital/lista")
@@ -50,7 +45,7 @@ public class SolicitacaoController {
 
         model.addAttribute("solicitacoesHospital", repositorioSolicitacao.findAll());
 
-        return "listaSolicitacao/listaSolicitacaoHospital";
+        return "solicitacao/listagens/listaSolicitacaoHospital";
     }
 
     @PostMapping("/cadastro")
@@ -58,6 +53,6 @@ public class SolicitacaoController {
     public String salvaSolicitacao(Solicitacao solicitacao) {
         repositorioSolicitacao.save(solicitacao);
 
-        return "salvou:" + solicitacao.getNome();
+        return "inicio.html" ;
     }
 }
