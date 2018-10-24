e8185941c397ae2f03fb803d24317b7c97f2545f
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2018-10-24 15:09:36 -0300

    <@rfelipe,@brendamoniqueb>ajustando o caminho da lista hospital

--- app/src/main/java/com/thoughtworks/aceleradora/solicitacao/controller/SolicitacaoController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/solicitacao/controller/SolicitacaoController.java
@@ -34,6 +34,22 @@ public class SolicitacaoController {
         return "solicitacao/cadastro";
     }
 
+    @GetMapping("/casa/lista")
+    public String listaSolicitacoesDaCasa(Model model) {
+
+        model.addAttribute("solicitacoesCasa", solicitacaoRepository.findAll());
+
+        return "solicitacao/listagens/listaSolicitacaoCasa";
+    }
+
+    @GetMapping("/hospital/lista")
+    public String listaSolicitacoesDoHospital(Model model) {
+
+        model.addAttribute("solicitacoesHospital", solicitacaoRepository.findAll());
+
+        return "solicitacao/listagens/listaSolicitacaoHospital";
+    }
+
     @PostMapping("/cadastro")
     @ResponseBody
     public String salvaSolicitacao(Model model, @ModelAttribute("solicitacao") Solicitacao solicitacao) {
