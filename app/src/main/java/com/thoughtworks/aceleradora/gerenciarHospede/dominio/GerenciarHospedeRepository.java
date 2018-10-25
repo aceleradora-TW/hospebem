47fc070c9111c4332076bbc144bbf65e2755c82c
laurascclaro <laurascclaro@hotmail.com>
2018-10-25 11:49:24 -0300

    <@rfelipe> renderiza lista da casa

---
+++ app/src/main/java/com/thoughtworks/aceleradora/gerenciarHospede/dominio/GerenciarHospedeRepository.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.aceleradora.gerenciarHospede.dominio;
+
+
+import com.thoughtworks.aceleradora.solicitacao.dominio.Solicitacao;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GerenciarHospedeRepository
+        extends CrudRepository<Solicitacao,Long> {
+
+}
