a7a6bc5ef3fe12792a754da72e24cc8c50e5cde3
matheus-malta <mmaatheeus@gmail.com>
2018-10-30 11:50:22 -0300

    <@marcos012, @matheus-malta> Adiciona campo vermelho

--- app/src/main/resources/static/js/validaCadastroSolicitacao.js
+++ app/src/main/resources/static/js/validaCadastroSolicitacao.js
@@ -11,45 +11,68 @@ function validaCadastroSolicitacao(evento) {
 }
 
 function camposPaciente() {
-    if (document.getElementById("nome").value == false) {
-        alert('Por favor, preencha o campo nome');
-        document.getElementById("nome").focus();
+    var nome =  document.getElementById("nome")
+    var genero1 = document.getElementById("genero1")
+    var genero2 = document.getElementById("genero2")
+    var dataNascimento =  document.getElementById("dataNascimento")
+    var peso = document.getElementById("peso")
+    var ehCadeirante = document.getElementById("cadeirante.sim")
+    var situacaoPre = document.getElementById("situacao.pre")
+    var situacaoPos = document.getElementById("situacao.pos")
+    var dataEntrada = document.getElementById("dataEntrada")
+
+
+    if (nome.value  === '') {
+        alert('Por favor, preencha o campo nome')
+        nome.classList.remove('input-fancy')
+        nome.classList.add('is-danger')
+        nome.focus()
+        return false
+    } else
+        nome.classList.add('input-fancy')
+
+
+    if (genero1.checked  === false && genero2.checked  === false) {
+        alert('Por favor, selecione o gênero do paciente')
+        genero1.focus();
         return false;
     }
 
-    if (document.getElementById("genero1").checked == false &&
-        document.getElementById("genero2").checked == false) {
-        alert('Por favor, selecione o gênero do paciente');
-        document.getElementById("genero1").focus();
-        return false;
-    }
+    if (dataNascimento.value  === '') {
+        alert('Por favor, preencha a data de nascimento')
+        dataNascimento.classList.remove('input-fancy')
+        dataNascimento.classList.add('is-danger')
+        dataNascimento.focus()
+        return false
+    } else
+        dataNascimento.classList.add('input-fancy')
 
-    if (document.getElementById("dataNascimento").value == false) {
-        alert('Por favor, preencha a data de nascimento');
-        document.getElementById("dataNascimento").focus();
-        return false;
-    }
-
-    if (document.getElementById("cadeirante.sim").checked == true) {
-        if (document.getElementById("peso").value == false) {
-            alert('Por favor, preencha o campo peso');
-            document.getElementById("peso").focus();
+    if (ehCadeirante.checked  === true) {
+        if (peso.value === '') {
+            alert('Por favor, preencha o campo peso')
+            peso.classList.remove('input-fancy')
+            peso.classList.add('is-danger')
+            peso.focus();
             return false;
-        }
+        } else
+            peso.classList.add('input-fancy')
     }
 
-    if (document.getElementById("situacao.pre").checked == false &&
-        document.getElementById("situacao.pos").checked == false) {
-        alert('Por favor, selecione a situacao do transplante');
-        document.getElementById("situacao.pre").focus();
+    if (situacaoPre.checked === false &&
+        situacaoPos.checked === false) {
+        alert('Por favor, selecione a situacao do transplante')
+        situacaoPre.focus()
         return false;
     }
 
-    if (document.getElementById("dataEntrada").value == false){
-        alert('Por favor, preencha a data de entrada');
-        document.getElementById("dataEntrada").focus();
-        return false;
-    }
+    if (dataEntrada.value === ''){
+        alert('Por favor, preencha a data de entrada')
+        dataEntrada.classList.remove('input-fancy')
+        dataEntrada.classList.add('is-danger')
+        dataEntrada.focus()
+        return false
+    } else
+        dataEntrada.classList.add('input-fancy')
 
     return true
 }
