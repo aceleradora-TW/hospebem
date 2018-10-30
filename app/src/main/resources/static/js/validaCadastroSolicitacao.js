edf9103308e7a9a7d5d8d567e988913905a9e65c
matheus-malta <mmaatheeus@gmail.com>
2018-10-30 13:08:37 -0300

    <@marcos012, @matheus-malta> Faz funcao de mudar cor do input

--- app/src/main/resources/static/js/validaCadastroSolicitacao.js
+++ app/src/main/resources/static/js/validaCadastroSolicitacao.js
@@ -21,39 +21,35 @@ function camposPaciente() {
     var situacaoPos = document.getElementById("situacao.pos")
     var dataEntrada = document.getElementById("dataEntrada")
 
-
     if (nome.value  === '') {
         alert('Por favor, preencha o campo nome')
-        nome.classList.remove('input-fancy')
-        nome.classList.add('is-danger')
-        nome.focus()
+        validaInputErrado(nome)
         return false
+
     } else
         nome.classList.add('input-fancy')
 
 
     if (genero1.checked  === false && genero2.checked  === false) {
         alert('Por favor, selecione o gênero do paciente')
-        genero1.focus();
-        return false;
+        genero1.focus()
+        return false
     }
 
     if (dataNascimento.value  === '') {
         alert('Por favor, preencha a data de nascimento')
-        dataNascimento.classList.remove('input-fancy')
-        dataNascimento.classList.add('is-danger')
-        dataNascimento.focus()
+        validaInputErrado(dataNascimento)
         return false
+
     } else
         dataNascimento.classList.add('input-fancy')
 
     if (ehCadeirante.checked  === true) {
         if (peso.value === '') {
             alert('Por favor, preencha o campo peso')
-            peso.classList.remove('input-fancy')
-            peso.classList.add('is-danger')
-            peso.focus();
-            return false;
+            validaInputErrado(peso)
+            return false
+
         } else
             peso.classList.add('input-fancy')
     }
@@ -62,15 +58,14 @@ function camposPaciente() {
         situacaoPos.checked === false) {
         alert('Por favor, selecione a situacao do transplante')
         situacaoPre.focus()
-        return false;
+        return false
     }
 
     if (dataEntrada.value === ''){
         alert('Por favor, preencha a data de entrada')
-        dataEntrada.classList.remove('input-fancy')
-        dataEntrada.classList.add('is-danger')
-        dataEntrada.focus()
+        validaInputErrado(dataEntrada)
         return false
+
     } else
         dataEntrada.classList.add('input-fancy')
 
@@ -78,23 +73,34 @@ function camposPaciente() {
 }
 
  function camposAcompanhantes(){
-     if (document.querySelector(".nome").value == false) {
-         alert('Por favor, preencha o campo nome do acompanhante 1');
-         document.querySelector(".nome").focus();
-         return false;
-     }
-     if (document.querySelectorAll(".genero")[0].checked == false &&
-        document.querySelectorAll(".genero")[1].checked == false) {
+     var nomeAcomp = document.querySelector(".nome")
+     var generoAcomp = document.querySelectorAll(".genero")
+     var dataNasciAcomp = document.querySelector(".data")
+
+     if (nomeAcomp.value === '') {
+         alert('Por favor, preencha o campo nome do acompanhante 1')
+         validaInputErrado(nomeAcomp)
+         return false
+
+     } else
+         nomeAcomp.classList.add('input-fancy')
+
+     if (generoAcomp[0].checked === false && generoAcomp[1].checked === false) {
          alert('Por favor, selecione o gênero do acompanhante 1');
          return false;
      }
-     if(document.querySelector(".data").value == false){
+
+     if(dataNasciAcomp.value == false){
          alert('Por favor, preencha a data de nascimento do acompanhante');
-         document.querySelector(".data").focus();
+         validaInputErrado(dataNasciAcomp)
          return false;
      }
 
      return true;
 }
 
-
+function validaInputErrado(item) {
+    item.classList.remove('input-fancy')
+    item.classList.add('is-danger')
+    item.focus()
+}
