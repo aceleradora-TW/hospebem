184fa030f16a4f6dfa8c06e83547039978bf4ee7
JulianeMartins <jumartinsvargas@gmail.com>
2018-11-20 15:10:52 -0200

    apaga comentários do código

--- app/src/main/java/com/thoughtworks/aceleradora/login/controllers/LoginController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/login/controllers/LoginController.java
@@ -66,15 +66,4 @@ public class LoginController {
     public String paginaAssistente() {
         return "/solicitacao/listagens/listaSolicitacaoHospital";
     }
-
-   /* @GetMapping("/logout")
-    public String getLogoutPage(HttpServletRequest request, HttpServletResponse response) {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null)
-            new SecurityContextLogoutHandler().logout(request, response, authentication);
-
-        return "redirect:/login";
-    }*/
-
 }
