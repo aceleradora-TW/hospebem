f88f920310734a2241a5cdb3ab535dc47f57449c
othman853 <anuar.yasser@gmail.com>
2018-11-21 13:00:02 -0200

    @othman853: Remove testes funcionais antigos

--- testes-funcionais/cypress/integration/home.test.js
+++ testes-funcionais/cypress/integration/home.test.js
@@ -1,17 +1,7 @@
 describe('A pagina inicial', () => {
 
-  it('carrega com sucesso', () => {
+  it('carrega tudo corretamente, incluindo recursos estaticos', () => {
     cy.visit('/')
-  })
-
-  it('tem um titulo', () => {
-    cy.get('h1').should('contain', 'Esqueleto Ambulante')
-  })
-
-  it('tem links para as paginas "inicio" e "pessoas" e "cadastrar pessoa"', () => {
-    cy.get('a > li').eq(0).should('contain', 'Início')
-    cy.get('a > li').eq(1).should('contain', 'Pessoas')
-    cy.get('a > li').eq(2).should('contain', 'Cadastrar Pessoa')
   })
 
 })
