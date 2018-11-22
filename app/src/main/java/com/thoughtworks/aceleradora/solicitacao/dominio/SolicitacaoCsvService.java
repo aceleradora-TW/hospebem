3be9d99225c7531402027b111875ad36a894f72d
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2018-11-22 15:57:05 -0200

    <@ingridDuarte, @vicfraga> ajusta controller para exportar os dados

--- app/src/main/java/com/thoughtworks/aceleradora/solicitacao/dominio/SolicitacaoCsvService.java
+++ app/src/main/java/com/thoughtworks/aceleradora/solicitacao/dominio/SolicitacaoCsvService.java
@@ -1,21 +1,39 @@
 package com.thoughtworks.aceleradora.solicitacao.dominio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.lang.String.join;
+import static java.util.stream.Collectors.joining;
 
 @Service
 public class SolicitacaoCsvService {
 
     private SolicitacaoRepository repository;
 
+    @Autowired
     public SolicitacaoCsvService(SolicitacaoRepository repository) {
         this.repository = repository;
     }
 
     public String solicitacoesNegadas() {
-        return "";
+         List<Solicitacao> negada = repository.findAll();
+
+        String listaSolicitacoesNegadas = negada
+                .stream()
+                .map(solicitacao -> join(solicitacao.getSituacao()).join(solicitacao.getOrgao()))
+                .collect(joining(","));
+
+
+        return listaSolicitacoesNegadas;
+
     }
 
     public String solicitacoesAceitas() {
-        return "";
+    return "";
     }
 }
