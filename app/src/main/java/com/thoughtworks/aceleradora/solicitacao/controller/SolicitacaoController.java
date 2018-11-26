f8d58c90fbf7556df0a56790e289b0e0e30adba7
matheus-malta <mmaatheeus@gmail.com>
2018-11-26 14:58:20 -0200

    <@rfelipe, @matheus-malta> lista solictacoes por login
    Co-authored-by: rfelipe <f.questao@gmail.com>

--- app/src/main/java/com/thoughtworks/aceleradora/solicitacao/controller/SolicitacaoController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/solicitacao/controller/SolicitacaoController.java
@@ -2,6 +2,8 @@ package com.thoughtworks.aceleradora.solicitacao.controller;
 
 import com.thoughtworks.aceleradora.solicitacao.dominio.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -53,6 +55,9 @@ public class SolicitacaoController {
 
     @GetMapping("/hospital/lista")
     public String listaSolicitacoesDoHospital(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+         auth.getName();
+         model.addAttribute("usuarioLogado",auth.getName());
 
         model.addAttribute("solicitacoesHospital", solicitacaoRepository.findAll());
 
