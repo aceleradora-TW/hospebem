e13f6b2d77790640aea7f20f35c5c12ce4bda599
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2018-11-29 15:51:42 -0200

    <@rfelipe, @ingridDuarte> Implementa lista de Usuarios

--- app/src/main/java/com/thoughtworks/aceleradora/login/controllers/LoginController.java
+++ app/src/main/java/com/thoughtworks/aceleradora/login/controllers/LoginController.java
@@ -2,26 +2,31 @@ package com.thoughtworks.aceleradora.login.controllers;
 
 import com.thoughtworks.aceleradora.login.dominio.Cargo;
 import com.thoughtworks.aceleradora.login.dominio.Usuario;
+import com.thoughtworks.aceleradora.login.dominio.UsuarioRepository;
 import com.thoughtworks.aceleradora.login.servicos.UsuarioService;
 import com.thoughtworks.aceleradora.login.validador.UsuarioValidador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class LoginController {
 
     private UsuarioValidador usuarioValidador;
     private UsuarioService usuarioService;
+    private UsuarioRepository usuarioRepository;
+
 
     @Autowired
-    public LoginController(UsuarioValidador usuarioValidador, UsuarioService usuarioService) {
+    public LoginController(UsuarioValidador usuarioValidador, UsuarioService usuarioService, UsuarioRepository usuarioRepository) {
         this.usuarioValidador = usuarioValidador;
         this.usuarioService = usuarioService;
+        this.usuarioRepository = usuarioRepository;
+
     }
 
     @GetMapping(value = "/registrar")
@@ -44,6 +49,28 @@ public class LoginController {
 
         return "registrarUsuario/usuarioSalvo";
     }
+    //@GetMapping("/listaUsuarios")
+    @RequestMapping(value = "/listaUsuarios", method = RequestMethod.GET)
+    public String buscarTodosUsuarios(Model model) {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+/*        for (int i = 0; i < usuarios.size(); i++) {
+            usuarios.remove(usuarios.get(i));
+
+        }*/
+        model.addAttribute("usuarios", usuarios);
+        //model.addAttribute("usuario",new Usuario());
+        //model.addAttribute("nomeUsuario",usuarioRepository.getNomeUsuarioLogado());
+        return "solicitacao/listagens/listaUsuarios";
+    }
+
+//    @RequestMapping(value = "{id}/editaUsuario", method = RequestMethod.GET)
+//    //@GetMapping("{id}/editaUsuario")
+//    public String editaUsuario(Model model, @PathVariable Long id) {
+//        Usuario usuario = usuarioRepository.findOneById(id);
+//        model.addAttribute("usuarios", usuario);
+//        return "/solicitacao/editaUsuario";
+//    }
+
 
     @GetMapping(value = "/login")
     public String login() {
--- app/src/main/java/com/thoughtworks/aceleradora/login/dominio/UsuarioRepository.java
+++ app/src/main/java/com/thoughtworks/aceleradora/login/dominio/UsuarioRepository.java
@@ -3,9 +3,16 @@ package com.thoughtworks.aceleradora.login.dominio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Usuario findByNome(String nome);
+    List<Usuario> findAll();
+    Usuario findOneByEmail(String email);
+    Usuario findOneById(Long Id);
+
+
 
 }
