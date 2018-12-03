565c77e8543784958b216ab9159c750bddf0fc17
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2018-12-03 16:20:47 -0200

    <@julianemartins, @ingridDuarte> Valida cargo do Usuário

---
+++ app/src/main/resources/static/js/validaUsuario.js
@@ -0,0 +1,14 @@
+function validaUsuario(event) {
+    if (validaTipoUsuario()){
+        event.currentTarget.submit()
+        return true
+    }
+    return false
+}
+
+
+function validarCamposUsuario() {
+    
+}
+
+}
